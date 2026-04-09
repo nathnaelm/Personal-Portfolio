845bdc65b27e61e73172ea87d29d7142196a0c4c
Nathnael Mecuria <130406421+nathnaelm@users.noreply.github.com>
2026-04-09 14:42:07 -0400

    Added LinkedIn + Github images and links, as well as the main tab bar.

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,40 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currentTab, setCurrentTab] = useState('home')
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="TabBar">
+          <button onClick={() => setCurrentTab('home')} className={currentTab === 'home' ? 'active' : ''}> Home </button>
+          <button onClick={() => setCurrentTab('work')} className={currentTab === 'work' ? 'active' : ''}> Work </button>
+          <button onClick={() => setCurrentTab('portfolio')} className={currentTab === 'portfolio' ? 'active' : ''}> Portfolio </button>
+          <button onClick={() => setCurrentTab('personal')} className={currentTab === 'personal' ? 'active' : ''}> Personal </button>
+          <button onClick={() => setCurrentTab('contact')} className={currentTab === 'contact' ? 'active' : ''}> Contact </button>
+        </div>
+        {/* <div className="content">
+          {currentTab === 'home' && <Home />}
+          {currentTab === 'work' && <Work />}
+          {currentTab === 'portfolio' && <Portfolio />}
+          {currentTab === 'personal' && <Personal />}
+          {currentTab === 'contact' && <Contact />}
+        </div> */}
+        <div >
+          <a href="https://www.linkedin.com/in/nathnael-mecuria/" target="_blank">
+            <img src={"LinkedInLogo.png"} className="LinkedIn logo" alt="LinkedIn logo" />
+          </a>
+          <a href="https://github.com/nathnaelm" target="_blank">
+            <img src={"GithubLogo.png"} className="Github logo" alt="Github logo" />
+          </a>
+        </div>
       </div>
       <h1>Welcome to my Portfolio!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
