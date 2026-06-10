21f291f46792cc6f151d66e13282442e2804fa8f
Nathnael Mecuria <130406421+nathnaelm@users.noreply.github.com>
2026-06-10 19:18:06 +0200

    Tweaked about me message and tab bar + routing now appearing/working properly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,24 @@
-import LinkedInLogo from '../LinkedInLogo.png'
-import GithubLogo from '../GithubLogo.png'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './tabs/Home/Home'
 import Work from './tabs/Work/Work'
 import Portfolio from './tabs/Portfolio/Portfolio'
 import Contact from './tabs/Contact/Contact'
+import TabBar from './components/TabBar'
 import './App.css'
 
 function App() {
-
   return (
-    <div>
-
-
-      {/* <Router> */}
-
-      {/* <TabBar /> */}
-
-      {/* <Routes>
+    <Router>
+      <div className="app-shell">
+        <TabBar />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
-        </Routes> */}
-
-      <div>
-
-        <div>
-          <a href="https://www.linkedin.com/in/nathnael-mecuria/" target="_blank" rel="noreferrer">
-            <img src={LinkedInLogo} className="LinkedIn logo" alt="LinkedIn logo" />
-          </a>
-          <a href="https://github.com/nathnaelm" target="_blank" rel="noreferrer">
-            <img src={GithubLogo} className="Github logo" alt="Github logo" />
-          </a>
-        </div>
-
+        </Routes>
       </div>
-
-      <h1>Welcome to my Portfolio!</h1>
-      <div className="aboutMecard">
-        <p>
-          Hi there! I'm Nathnael Mecuria, a curious and driven computer science senior at Virginia Tech with an interest in software engineering, particularly front-end as well as app development.
-          I enjoy creating intuitive, user-focused applications that balance clean design with strong technical foundations.
-          I bring a problem-solving mindset, attention to detail, and a strong desire to continuously learn and improve within collaborative team environments.
-        </p>
-      </div>
-      {/* </Router> */}
-    </div>
+    </Router>
   )
 }
 
--- src/tabs/Home/home.jsx
+++ src/tabs/Home/home.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import LinkedInLogo from '../../../LinkedInLogo.png'
+import GithubLogo from '../../../GithubLogo.png'
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <p>Welcome to my portfolio website.</p>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <a href="https://www.linkedin.com/in/nathnael-mecuria/" target="_blank" rel="noreferrer">
+          <img src={LinkedInLogo} className="LinkedIn logo" alt="LinkedIn logo" />
+        </a>
+        <a href="https://github.com/nathnaelm" target="_blank" rel="noreferrer">
+          <img src={GithubLogo} className="Github logo" alt="Github logo" />
+        </a>
+      </div>
+
+      <h1>Welcome to my Portfolio!</h1>
+      <div className="aboutMecard">
+        <p>
+          Hi there! I'm Nathnael Mecuria, a curious and driven computer science recent graduate and incoming Master's student at Virginia Tech with an interest in software engineering, particularly front-end as well as app development.
+          I enjoy creating intuitive, user-focused applications that balance clean design with strong technical foundations.
+          I bring a problem-solving mindset, attention to detail, and a strong desire to continuously learn and improve within collaborative team environments.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Home
